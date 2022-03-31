b987a5fed03eafc20c623febc046c42d0e3955f2
Jacob Nesbitt <jjnesbitt2@gmail.com>
2022-03-31 18:40:53 -0400

    Add button to assetstore views

--- import_tracker/web_client/main.js
+++ import_tracker/web_client/main.js
@@ -1,1 +1,15 @@
-console.log('Loaded import_tracker!');
+import AssetstoreView from '@girder/core/views/body/AssetstoresView';
+import { wrap } from '@girder/core/utilities/PluginUtils';
+
+import importDataButton from './templates/assetstoreButtonsExtension.pug';
+
+wrap(AssetstoreView, 'render', function (render) {
+    // Call the underlying render function that we are wrapping
+    render.call(this);
+
+    // Inject new button into each assetstore
+    const assetstores = this.collection.toArray();
+    this.$('.g-assetstore-import-button-container').after(
+        (i) => importDataButton({ assetstore: assetstores[i] })
+    );
+});
