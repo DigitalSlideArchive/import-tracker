af86c6cc357dd933acae4187fd6e05d0e93fa545
Jacob Nesbitt <jjnesbitt2@gmail.com>
2022-04-01 14:20:00 -0400

    Add page to view and re-perform imports

--- import_tracker/web_client/main.js
+++ import_tracker/web_client/main.js
@@ -1,8 +1,12 @@
 import AssetstoreView from '@girder/core/views/body/AssetstoresView';
 import { wrap } from '@girder/core/utilities/PluginUtils';
+import events from '@girder/core/events';
+import router from '@girder/core/router';
 
 import importDataButton from './templates/assetstoreButtonsExtension.pug';
+import importListView from './views/importList';
 
+// Inject button to navigate to imports page in each assetstore in view
 wrap(AssetstoreView, 'render', function (render) {
     // Call the underlying render function that we are wrapping
     render.call(this);
@@ -10,6 +14,11 @@ wrap(AssetstoreView, 'render', function (render) {
     // Inject new button into each assetstore
     const assetstores = this.collection.toArray();
     this.$('.g-assetstore-import-button-container').after(
-        (i) => importDataButton({ assetstore: assetstores[i] })
+        (i) => importDataButton({ importsPageLink: `#assetstore/${assetstores[i].id}/imports` })
     );
+});
+
+// Setup router to assetstore imports view
+router.route('assetstore/:id/imports', 'importsPage', function (id) {
+    events.trigger('g:navigateTo', importListView, { id });
 });
---
+++ import_tracker/web_client/views/importList.js
@@ -0,0 +1,51 @@
+import $ from 'jquery';
+
+import AssetstoreModel from '@girder/core/models/AssetstoreModel';
+import View from '@girder/core/views/View';
+import router from '@girder/core/router';
+import { restRequest } from '@girder/core/rest';
+
+import importListTemplate from '../templates/importList.pug';
+import '../stylesheets/importList.styl';
+
+var importList = View.extend({
+    events: {
+        'click .re-import-btn': function (e) {
+            const index = Number($(e.currentTarget).attr('index'));
+            const importEvent = this.imports[index];
+            if (importEvent === undefined) {
+                return;
+            }
+
+            // Re-perform import
+            const assetstore = new AssetstoreModel({ _id: this.assetstoreId });
+            const destType = importEvent.params.destinationType;
+            const destId = importEvent.params.destinationId;
+            assetstore.off('g:imported').on('g:imported', function () {
+                router.navigate(destType + '/' + destId, { trigger: true });
+            }, this).on('g:error', function (resp) {
+                this.$('.g-validation-failed-message').text(resp.responseJSON.message);
+            }, this).import(importEvent.params);
+        }
+    },
+
+    initialize: function ({ id }) {
+        this.assetstoreId = id;
+        this.imports = [];
+        restRequest({
+            url: `assetstore/${id}/imports`,
+            method: 'GET'
+        }).done((result) => {
+            this.imports = result;
+            this.render();
+        });
+    },
+
+    render: function () {
+        this.$el.html(importListTemplate({ imports: this.imports }));
+
+        return this;
+    }
+});
+
+export default importList;
