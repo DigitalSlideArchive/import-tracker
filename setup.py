49827a5bbc3086b4e499910b2a4ab39460011f3f
Jacob Nesbitt <jjnesbitt2@gmail.com>
2022-04-11 14:17:53 -0400

    Fix long_description_content_type

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     install_requires=requirements,
     license='Apache Software License 2.0',
     long_description=readme,
-    long_description_content_type='text/x-rst',
+    long_description_content_type='text/markdown',
     include_package_data=True,
     keywords='girder-plugin, import_tracker',
     name='import_tracker',
