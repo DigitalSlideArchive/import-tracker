ed421c95833d61103316d4d7af042ac16e454023
Jacob Nesbitt <jjnesbitt2@gmail.com>
2022-04-11 14:00:28 -0400

    Fix quotes

--- import_tracker/rest.py
+++ import_tracker/rest.py
@@ -9,13 +9,13 @@ from bson.objectid import ObjectId
 
 @access.admin
 @autoDescribeRoute(
-    Description("List all imports for a given assetstore.")
-    .param("id", "", "path")
-    .pagingParams(defaultSort="started", defaultSortDir=SortDir.DESCENDING)
+    Description('List all imports for a given assetstore.')
+    .param('id', '', 'path')
+    .pagingParams(defaultSort='started', defaultSortDir=SortDir.DESCENDING)
 )
 def listImports(id, limit, offset, sort):
     return AssetstoreImport().find(
-        {"assetstoreId": ObjectId(id)},
+        {'assetstoreId': ObjectId(id)},
         limit=limit,
         offset=offset,
         sort=sort,
