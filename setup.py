59f93c7010d7bd25a95754a45be407b46a8ce544
Jacob Nesbitt <jjnesbitt2@gmail.com>
2022-04-11 13:55:55 -0400

    Fix incorrect file name

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-with open("README.rst") as readme_file:
+with open("README.md") as readme_file:
     readme = readme_file.read()
 
 requirements = ["girder>=3.0.0a1"]
